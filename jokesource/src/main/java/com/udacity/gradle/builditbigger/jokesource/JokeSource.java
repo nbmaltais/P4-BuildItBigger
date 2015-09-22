752a96dd017a062a886bea8edac3ed531e873dec
nbmaltais <nbmaltais@gmail.com>
2015-09-21 22:32:51 -0400

    Minor reformating. Added joke library.

---
+++ jokesource/src/main/java/com/udacity/gradle/builditbigger/jokesource/JokeSource.java
@@ -0,0 +1,25 @@
+package com.udacity.gradle.builditbigger.jokesource;
+
+import java.util.Random;
+
+public class JokeSource {
+
+    private String[] mJokes = new String[]{
+            "Joke 1. Hahahaha",
+            "Joke 2. HAHAHAHAHA",
+            "Joke 3. HAHAHAHAHA",
+            "Joke 4. HAHAHAHAHA",
+            "Joke 5. HAHAHAHAHA"
+            };
+
+    public JokeSource() {
+    }
+
+    public String tellJoke()
+    {
+        double i = Math.random();
+        if(i<0) i=-i;
+        int idx = (int)i % mJokes.length;
+        return mJokes[idx];
+    }
+}
